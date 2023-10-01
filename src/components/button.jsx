61abd8ae13c336d90d77e7c91ef3481aab76d9bf
Linkoln195 <gilioko195@gmail.com>
2023-10-01 13:39:00 +0200

    loading await

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,16 +2,25 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import buttonStyle from './button.module.css'
 import clickerStyles from './clicker.module.css'
+import Loading from './loading.jsx'
 import Text from './text.jsx'
 
 const Button = () => {
 	const [count, setCount] = useState(0)
+	const [state, setState] = useState('loading')
+
+	const getClicksCount = async () => {
+		await axios
+			.get('https://clickerbd.onrender.com/get-clicks')
+			.then(response => {
+				setCount(response.data)
+				setState('loaded')
+				console.log(response.data)
+			})
+	}
 
 	useEffect(() => {
-		axios.get('https://clickerbd.onrender.com/get-clicks').then(response => {
-			setCount(response.data)
-			console.log(response.data)
-		})
+		getClicksCount()
 	}, [])
 
 	const onButtonClick = async () => {
@@ -26,14 +35,22 @@ const Button = () => {
 		setCount(count + 1)
 	}
 
-	return (
-		<div className={clickerStyles.clicker}>
-			<Text textContent={count} />
-			<button className={buttonStyle.button} onClick={onButtonClick}>
-				Click
-			</button>
-		</div>
-	)
+	if (state == 'loading') {
+		return (
+			<div className={clickerStyles.clicker}>
+				<Loading />
+			</div>
+		)
+	} else if (state == 'loaded') {
+		return (
+			<div className={clickerStyles.clicker}>
+				<Text textContent={count} />
+				<button className={buttonStyle.button} onClick={onButtonClick}>
+					Click
+				</button>
+			</div>
+		)
+	}
 }
 
 export default Button
---
+++ src/components/loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './loading.module.css'
+
+const Loading = () => {
+	return <h1 className={classes.loadingText}>Loading...</h1>
+}
+
+export default Loading
